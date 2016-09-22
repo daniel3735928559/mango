4a6abe5893331a0722abce8e00da984686bc76e5
DR <daniel3735928559@gmail.com>
2016-09-22 05:05:42 +0000

    starting C; js bugfix

--- libmango/c/libmango.c
+++ libmango/c/libmango.c
@@ -1,84 +1,114 @@
-zmq = require('zmq');
-jsyaml = require('js-yaml');
-fs = require('fs');
+#include "transport.h"
+#include "serialiser.h"
+#include "dataflow.h"
+#include "interface.h"
+#include "args.h"
+#include "error.h"
 
-function MNode(debug){
-    var self = this;
-    this.version = "0.1";
-    this.debug = debug;
+char *LIBMANGO_VERSION = "0.1";
+
+typedef struct m_node {
+  char *version;
+  char *node_id;
+  uint32 mid;
+  char **ports;
+  char debug;
+  char *server_addr;
+  m_interface_t *interface;
+  m_serialiser_t *serialiser;
+  m_transport_t *local_gateway;
+  m_dataflow_t *dataflow;
+} m_node_t;
+
+void m_node_new(char debug){
+  m_node_t *n = malloc(sizeof(m_node_t));
+  n->version = LIBMANGO_VERSION;
+  n->debug = debug;
+  n->node_id = NULL;//...
+  n->mid = 0;
+  /*
     this.serialiser = new Serialiser(this.version);
     this.iface = new Interface();
     this.node_id = process.env['MANGO_ID'];
     this.mid = 0;
     this.ports = [];
     var server = process.env['MC_ADDR'];
-    
-    this.ready = function(header,args){
-	var ifce = {};
-	for(var i in this.iface.iface){
-	    ifce[i] = JSON.parse(JSON.stringify(this.iface.iface[i]));
-	    delete ifce[i]['handler'];
-	}
-	console.log("IF",ifce)
-	self.m_send('hello',{'id':self.node_id,'if':ifce,'ports':self.ports},"reg",null,"mc")
-    }
-    
-    this.dispatch = function(header,args){
-	console.log("DISPATCH",header,args,self.iface.iface,self.iface.iface[header['command']]);
-	try{
-            result = self.iface.iface[header['command']]['handler'](header,args);
-            if(result && 'callback' in header){
-		self.m_send(header['callback'],result,null,header['mid'],header['port'])
-	    }
-	} catch(e) {
-	    console.log(e);
-            self.handle_error(header['src_node'],e+"")
-	}
-    }
-    
-    this.handle_error = function(src,err){
-	console.log('OOPS',src,err);
-	self.m_send('error',{'source':src,'message':err},null,null,"mc");
-    }
-
-    this.reg = function(header,args){
-	self.node_id = args["id"];
-    }
-
-    this.reply = function(header,args){
-	console.log("REPLY",header,args);
-    }
-
-    this.heartbeat = function(header,args){
-	self.m_send("alive",{},null,null,"mc");
-    }
-
-    this.make_header = function(command,callback,mid,src_port){
-	if(!callback) callback = "reply";
-	if(!mid) mid = self.get_mid();
-	if(!src_port) src_port = "stdio";
-	return {'src_node':self.node_id, 'src_port':src_port, 'mid':mid, 'command':command, 'callback': callback};
-    }
-
-    this.get_mid = function(){
-	return self.mid++;
-    }
-
-    this.m_send = function(command,msg,callback,mid,port){
-	console.log('sending',command,msg,mid,port)
-	header = self.make_header(command,callback,mid,port)
-	self.dataflow.send(header,msg)
-	return header['mid']
-    }
-    
-    this.iface.add_interface('/home/zoom/suit/mango/libmango/node_if.yaml',{
+this.iface.add_interface('/home/zoom/suit/mango/libmango/node_if.yaml',{
         'reg':self.reg, 'reply':self.reply, 'heartbeat':self.heartbeat
     });
     this.local_gateway = new Transport(server);
     var s = this.local_gateway.socket;
     this.dataflow = new Dataflow(self.iface, self.local_gateway, self.serialiser, self.dispatch, self.handle_error);
     s.on('message',function(data){ self.dataflow.recv(data); });
+
+  */
+  return n;
 }
+
+void m_node_ready(m_node_t *node, m_header_t *header, m_args_t *args){
+	/* var ifce = {}; */
+	/* for(var i in this.iface.iface){ */
+	/*     ifce[i] = JSON.parse(JSON.stringify(this.iface.iface[i])); */
+	/*     delete ifce[i]['handler']; */
+	/* } */
+	/* console.log("IF",ifce) */
+	/* self.m_send('hello',{'id':self.node_id,'if':ifce,'ports':self.ports},"reg",null,"mc") */
+}
+
+void m_node_dispatch(m_node_t *node, m_header_t *header, m_args_t *args){
+	/* console.log("DISPATCH",header,args,self.iface.iface,self.iface.iface[header['command']]); */
+	/* try{ */
+        /*     result = self.iface.iface[header['command']]['handler'](header,args); */
+        /*     if(result && 'callback' in header){ */
+	/* 	self.m_send(header['callback'],result,null,header['mid'],header['port']) */
+	/*     } */
+	/* } catch(e) { */
+	/*     console.log(e); */
+        /*     self.handle_error(header['src_node'],e+"") */
+	/* } */
+}
+
+void m_node_dispatch(m_node_t *node, char *src, char *err){
+  /* console.log('OOPS',src,err); */
+  /* self.m_send('error',{'source':src,'message':err},null,null,"mc"); */
+}
+
+void m_node_reg(m_node_t *node, m_header_t *header, m_args_t *args){
+  //self.node_id = args["id"];
+}
+
+void m_node_reply(m_node_t *node, m_header_t *header, m_args_t *args){
+  //console.log("REPLY",header,args);
+}
+
+void m_node_heartbeat(m_node_t *node, m_header_t *header, m_args_t *args){
+  //self.m_send("alive",{},null,null,"mc");
+}
+
+m_header_t *m_node_heartbeat(m_node_t *node, char *command, char *callback, int mid, char *src_port){
+  if(!callback) callback = LIBMANGO_REPLY;
+  if(!mid) mid = m_node_get_mid(node);
+  if(!src_port) src_port = LIBMANGO_STDIO;
+  m_header_t *header = m_header_new();
+  header->src_node = node->node_id;
+  header->src_port = src_port;
+  header->mid = mid;
+  header->command = command;
+  header->callback = callback;
+  return header;
+}
+    
+int m_node_get_mid(m_node_t *node){
+  return node->mid++;
+}
+
+int m_node_send(m_node_t *node, char *command, m_args_t *msg, char *callback, int mid, char *port){
+  /* console.log('sending',command,msg,mid,port) */
+  /*   header = self.make_header(command,callback,mid,port) */
+  /*   self.dataflow.send(header,msg) */
+  /*   return header['mid'] */
+}
+    
 
 function MError(message){
     this.message = message;
@@ -196,22 +226,3 @@ function Serialiser(version){
 }
 
 module.exports = MNode;
-
-// function Excite(){
-//     process.env['MANGO_ID'] = 'exc';
-//     process.env['MC_ADDR'] = 'tcp://localhost:61453';
-//     this.node = new MNode();
-//     this.node.iface.add_interface('/home/zoom/suit/mango/nodes/excite/excite.yaml',
-// 				  {'excite':this.excite,'print':this.print});
-//     this.node.ready();
-// }
-
-// Excite.prototype.excite = function(header,args){
-//     return {'excited':args['str']+'!'}
-// }
-
-// Excite.prototype.print = function(header,args){
-//     console.log("PRINT",header,args);
-// }
-
-// var ex = new Excite();
