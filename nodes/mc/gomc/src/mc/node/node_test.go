e82000a137cc102cd9276547ecefd8d13f9b76c4
DR <daniel3735928559@gmail.com>
2020-03-18 19:55:45 -0400

    exec node tests

--- nodes/mc/gomc/src/mc/node/node_test.go
+++ nodes/mc/gomc/src/mc/node/node_test.go
@@ -58,3 +58,81 @@ func TestMergeNode(t *testing.T) {
 		}
 	}
 }
+
+type TestTransport struct {
+	Sent []serializer.Msg
+}
+
+func (t *TestTransport) RunServer() {
+
+}
+
+func (t *TestTransport) Tx(identity string, m serializer.Msg) error {
+	t.Sent = append(t.Sent, m)
+	return nil
+}
+
+func TestExecNode(t *testing.T) {
+	tr := &TestTransport{
+		Sent: make([]serializer.Msg, 0)}
+	
+	execnode := MakeExecNode("test","exectest", "testtype", "echo hi", tr)
+	
+	execnode.SendToNode(serializer.Msg{
+		Sender:"test",
+		MessageId:"m1",
+		Command:"test_cmd",
+		Cookie:"cookie1",
+		Data:map[string]interface{}{"arg1":"val1"}})
+	execnode.SendToNode(serializer.Msg{
+		Sender:"test",
+		MessageId:"m2",
+		Command:"test_cmd",
+		Cookie:"cookie2",
+		Data:map[string]interface{}{"arg2":"val2"}})
+	execnode.SendToNode(serializer.Msg{
+		Sender:"test",
+		MessageId:"m3",
+		Command:"test_cmd",
+		Cookie:"cookie3",
+		Data:map[string]interface{}{"arg3":"val3"}})
+	if len(execnode.TransportQueue) != 3 {
+		t.Errorf("Expected transport queue of length 3, got %d", len(execnode.TransportQueue))
+	}
+	if len(tr.Sent) != 0 {
+		t.Errorf("Expected sent array of length 0, got %d", len(tr.Sent))
+	}
+	
+	execnode.Start("testserver")
+	
+	if len(execnode.TransportQueue) != 3 {
+		t.Errorf("Expected transport queue of length 3, got %d", len(execnode.TransportQueue))
+	}
+	if len(tr.Sent) != 0 {
+		t.Errorf("Expected sent array of length 0, got %d", len(tr.Sent))
+	}
+	
+	execnode.GotAlive("testid",tr)
+	
+	if len(execnode.TransportQueue) != 0 {
+		t.Errorf("Expected transport queue of length 0, got %d", len(execnode.TransportQueue))
+	}
+	if len(tr.Sent) != 3 {
+		t.Errorf("Expected sent array of length 3, got %d", len(tr.Sent))
+	}
+	
+	execnode.SendToNode(serializer.Msg{
+		Sender:"test",
+		MessageId:"m4",
+		Command:"test_cmd",
+		Cookie:"cookie4",
+		Data:map[string]interface{}{"arg4":"val4"}})
+
+	
+	if len(execnode.TransportQueue) != 0 {
+		t.Errorf("Expected transport queue of length 0, got %d", len(execnode.TransportQueue))
+	}
+	if len(tr.Sent) != 4 {
+		t.Errorf("Expected sent array of length 4, got %d", len(tr.Sent))
+	}
+}
