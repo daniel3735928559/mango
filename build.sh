47287330e91b35241d418bee1f597ecaeddd763e
DR <daniel3735928559@gmail.com>
2020-03-26 20:28:35 -0400

    cleanup

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
-./bin/goyacc -o src/mc/route/parser.go -p RouteParser -v src/mc/route/parser.output src/mc/route/parser.go.y && \
-    ./bin/goyacc -o src/mc/valuetype/parser.go -p ValueTypeParser -v src/mc/valuetype/parser.output src/mc/valuetype/parser.go.y && \
-    ./bin/goyacc -o src/mc/value/parser.go -p ValueParser -v src/mc/value/parser.output src/mc/value/parser.go.y && \
-    echo hi
-GOPATH=$(realpath .) go build mc
-GOPATH=$(realpath .) go build nodes/excite
-GOPATH=$(realpath .) go build nodes/log
-GOPATH=$(realpath .) go build nodes/tester
-GOPATH=$(realpath .) go build nodes/notify
-GOPATH=$(realpath .) go build nodes/mx/agent
-GOPATH=$(realpath .) go build nodes/mx
-GOPATH=$(realpath .) go build nodes/imap
+./bin/goyacc -o src/mc/route/parser.go -p RouteParser -v src/mc/route/parser.output src/mc/route/parser.go.y
+./bin/goyacc -o src/mc/valuetype/parser.go -p ValueTypeParser -v src/mc/valuetype/parser.output src/mc/valuetype/parser.go.y
+./bin/goyacc -o src/mc/value/parser.go -p ValueParser -v src/mc/value/parser.output src/mc/value/parser.go.y
+GOPATH=$(realpath .) go install mc
+GOPATH=$(realpath .) go install nodes/excite
+GOPATH=$(realpath .) go install nodes/log
+GOPATH=$(realpath .) go install nodes/tester
+GOPATH=$(realpath .) go install nodes/notify
+GOPATH=$(realpath .) go install nodes/mx/agent
+GOPATH=$(realpath .) go install nodes/mx
+GOPATH=$(realpath .) go install nodes/imap
